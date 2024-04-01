3a9c878b59eb21cee8ca8d8dddc2ae56e7fa8f27
priyamathi25 <165410724+priyamathi25@users.noreply.github.com>
2024-04-01 11:07:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Variable declaration practice
+var num1 = 5;
+var num2 = 10;
+
+// Addition practice
+var sum = num1 + num2;
+
+// String variable practice
+var greeting = "Hello";
+
+// Displaying results practice
+console.log("Sum:", sum);
+console.log(greeting + " World!");
